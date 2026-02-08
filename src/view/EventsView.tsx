b8b573b5464683c676d58d0c41c6ea75cec38dae
Abdullah Rafeq <abdullah-rafeq@hotmail.com>
2026-02-08 02:14:02 +0100

    Fix the design

--- src/view/EventsView.tsx
+++ src/view/EventsView.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import EventsHeader from "@/components/EventsHeader";
+import PreviousEvent from "@/components/PreviousEvent";
 import { useTranslations } from "next-intl";
 
 export default function EventsView() {
   const t = useTranslations("Events");
 
   return (
-    <main className="mx-auto max-w-[980px] px-5 pb-20 pt-10 sm:px-8 lg:pb-28 lg:pt-16">
-          <EventsHeader
-        title={t("title")}
-        description={t("description")}
-      />
-    </main>
+    <>
+      <main className="mx-auto max-w-[980px] px-5 pb-20 pt-10 sm:px-8 lg:pb-28 lg:pt-16">
+            <EventsHeader
+          title={t("title")}
+          description={t("description")}
+        />
+      </main>
+      <PreviousEvent/>
+    </>
   );
 }
 
