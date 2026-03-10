50151255cd96325ac28aff670c5a5aecdc3e82b9
Abdullah Rafeq <abdullah-rafeq@hotmail.com>
2026-03-10 12:14:11 +0100

    Create overall layout of component. What is left is responsive design.

--- src/components/PreviousEvent.tsx
+++ src/components/PreviousEvent.tsx
@@ -6,16 +6,22 @@ import YearToggle from "./YearToggle";
 export default function PreviousEvent () {
     const t = useTranslations("Events");
     return (
-        <>
+        <div className="flex flex-col items-center ">
+            <h2 className="font-serif text-[64px] leading-[42px] font-bold tracking-[0] mb-[131px]">
+                {t("previous.title")}
+            </h2>
+            <p className="font-serif text-[40px] leading-[40px] font-normal tracking-[0] mb-[177px]">
+                {t("previous.description")}
+            </p>
             <YearToggle   
                 years={[2025, 2024, 2023]}
                 onChange={(year) => console.log(year)}
             />
             <div className="flex justify-center gap-4">
-                <Event title={t("previous.name")} description={t("previous.description")}/>
-                <Event title={t("previous.name")} description={t("previous.description")}/>
-                <Event title={t("previous.name")} description={t("previous.description")}/>
+                <Event title={t("previous.card1.name")} description={t("previous.card1.description")}/>
+                <Event title={t("previous.card2.name")} description={t("previous.card2.description")}/>
+                <Event title={t("previous.card3.name")} description={t("previous.card3.description")}/>
             </div>
-        </>
+        </div>
     )
 }
