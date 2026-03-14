e8db607d1e75dbc87d2f04c5258e892abe915b79
Ilyes Rouibi <ilyes.rouibi@epfl.ch>
2026-03-14 12:16:16 +0100

    feat: add a bit of css to the file

--- src/view/SupabaseView.tsx
+++ src/view/SupabaseView.tsx
@@ -20,32 +20,39 @@ export default function SupabaseView({
   onDeleteSubscription,
 }: SupabaseViewProps) {
   return (
-    <>
-      <h1>Add a subscription to the database:</h1>
-      <form action={onAddSubscription}>
-        <input
-          name="username"
-          type="text"
-          placeholder="Add a username"
-          required
-        />
-        <input name="email" type="email" placeholder="Add an email" required />
-        <Button type="submit">Submit</Button>
-      </form>
-      <h1>Current subscriptions:</h1>
-      <ul>
-        {subscriptions?.map((subscription) => (
-          <li key={subscription.id}>
-            {subscription.username} | {subscription.email}
-            <Button
-              onClick={() => onDeleteSubscription(subscription.id)}
-              type="button"
-            >
-              Delete
-            </Button>
-          </li>
-        ))}
-      </ul>
-    </>
+    <section className="flex flex-row items-center justify-center gap-6">
+      <div className="flex flex-col items-start gap-4">
+        <h1>Add a subscription to the database:</h1>
+        <form action={onAddSubscription}>
+          <input
+            name="username"
+            type="text"
+            placeholder="Add a username"
+            required
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Add an email"
+            required
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+        <h1>Current subscriptions:</h1>
+        <ul>
+          {subscriptions?.map((subscription) => (
+            <li key={subscription.id}>
+              {subscription.username} | {subscription.email}
+              <Button
+                onClick={() => onDeleteSubscription(subscription.id)}
+                type="button"
+              >
+                Delete
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
