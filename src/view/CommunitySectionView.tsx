f33b2531ec64ba497b1ba171f7218bb9712d046e
Abdullah Rafeq <abdullah-rafeq@hotmail.com>
2026-05-25 03:07:53 +0200

    Make right side herosection envelope responsive

--- src/view/CommunitySectionView.tsx
+++ src/view/CommunitySectionView.tsx
@@ -9,7 +9,6 @@ export default function CommunitySectionView() {
     <section className="px-6 sm:px-10 lg:px-[62px] pb-20 lg:pb-0 bg-gradient-to-b from-white to-[#8ECAFF]">
       <div className="relative w-full max-w-[1400px] mx-auto overflow-hidden lg:overflow-visible">
         <div className="flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-12 lg:gap-20">
-          
           {/* LEFT SIDE */}
           <div className="w-full max-w-[665px] text-left lg:text-left">
             <h2 className="w-full mt-0 lg:mt-[360px] font-[Nanum_Myeongjo] text-[32px] sm:text-[36px] lg:text-[40px] leading-[38px] sm:leading-[40px] lg:leading-[42px] font-bold">
@@ -26,31 +25,12 @@ export default function CommunitySectionView() {
               </button>
             </div>
           </div>
-
           {/* RIGHT SIDE */}
-          <div className="relative w-[320px] sm:w-[430px] lg:w-[500px] h-[620px] sm:h-[760px] lg:h-[900px] shrink-0">
+          <div className="lg:mt-[460px] mt-[150px] lg:mr-[100px] relative shrink-0">
             <img
-              src="/rectangle.png"
-              alt="rectangle"
-              className="absolute top-[120px] sm:top-[250px] lg:top-[402px] left-[-45px] sm:left-[-80px] lg:left-[-118px] z-[100] w-[170px] sm:w-auto"
-            />
-
-            <img
-              src="/rectangle2.png"
-              alt="rectangle"
-              className="absolute top-[250px] sm:top-[420px] lg:top-[574px] left-[80px] sm:left-[90px] lg:left-[93px] z-[100] w-[170px] sm:w-auto"
-            />
-
-            <img
-              src="/hero-pattern.svg"
+              src={"/herosection.svg"}
               alt=""
-              className="absolute top-[90px] sm:top-[210px] lg:top-[350px] left-[65px] sm:left-[75px] w-[220px] sm:w-[300px] lg:w-[339px] h-auto z-[90]"
-            />
-
-            <img
-              src="/darkone.png"
-              alt=""
-              className="absolute top-[210px] sm:top-[340px] lg:top-[478px] left-[130px] sm:left-[180px] lg:left-[204px] w-[220px] sm:w-[300px] lg:w-[339px] h-auto z-[80]"
+              className="w-[320px] sm:w-[470px] lg:w-[600px] h-auto shrink-0"
             />
           </div>
         </div>
