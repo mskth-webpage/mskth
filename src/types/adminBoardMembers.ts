b87ec318c45248876fabed6e6d644b6304cb23af
Seerat Yasir <seeratyasir@hotmail.com>
2026-07-17 07:49:01 +0200

    Update adminBoardMembers.ts
    Added for public view. Manually committing because of issues

--- src/types/adminBoardMembers.ts
+++ src/types/adminBoardMembers.ts
@@ -1,3 +1,11 @@
+export type PublicBoardMember = {
+  id: number;
+  name: string;
+  role: string;
+  email: string;
+  image_url: string | null;
+};
+
 export type AdminBoardMember = {
   id: number | `temp-${string}`;
   name: string;
